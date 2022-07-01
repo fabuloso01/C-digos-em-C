e39703f35720b966faaf38dd8b1d66fef655bc24
fabuloso01 <fabuloso01@github.com>
2022-06-30 22:57:19 -0400

    One commit

---
+++ Dankicode.c
@@ -0,0 +1,32 @@
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ano_nasc = 1980;
+    int ano_atual = 2022;
+
+    int resultado = ano_atual - ano_nasc;
+
+    printf("O Fábio tem %d anos", resultado);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float peso_atual = 85;
+    float altura_atual = 1.78;
+
+    int resultado = peso_atual / (altura_atual * altura_atual);
+    
+    printf(" O IMC do Fábio é %d", resultado);
+
+    return 0;
+}
