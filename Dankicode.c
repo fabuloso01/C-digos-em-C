938f09da0d0b7dc1aa1ad9bdb144d1215a802b41
fabuloso01 <fabuloso01@github.com>
2022-07-01 14:21:54 -0400

    Two commit

--- Dankicode.c
+++ Dankicode.c
@@ -15,7 +15,8 @@ int main()
     return 0;
 }
 */
-'''
+
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,4 +31,26 @@ int main()
 
     return 0;
 }
-'''
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int hora_cinema = 20;
+    int hora_atual = 20;
+
+    if(hora_atual > hora_cinema + 30) {
+        printf("Passou do tempo limite! Não pode entrar!");
+    }else if (hora_atual < hora_cinema - 30) {
+        printf("Nem abriu a porta do cinema"); 
+    }else{
+        printf("O horario está
+         certo! Pode entrar!");
+    }   
+    
+
+    return 0;
+}
